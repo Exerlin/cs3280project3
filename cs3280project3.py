0dde81db3297694eb7abaf89532f591f2a387556
exerlin <ejensen1@my.westga.edu>
2020-11-01 14:51:38 -0500

    Full functionality NOT TESTED

--- cs3280project3.py
+++ cs3280project3.py
@@ -13,20 +13,27 @@ __version__ = "Fall 2020"
 def scan(ip_address, start_port, end_port):
     queue = mp.Queue()
     number_of_ports = 1 + int(end_port) - int(start_port) 
-    processes = [mp.Process(target=check_for_open_port, args=(ip_address, str(int(start_port) + x))) for x in range(number_of_ports)]
+    processes = [mp.Process(target=check_for_open_port, args=(ip_address, str(int(start_port) + x), queue)) for x in range(number_of_ports)]
 
+    list_of_results = []
     for process in processes:
         process.start()
 
     for process in processes:
         process.join()
 
-def check_for_open_port(ip_address, port):
-    print("h")
+    if queue.empty():
+        print("No ports within the specified range are open.")
+    else:
+        print("Open ports:")
+        for port in queue.get():
+            print("\n" + str(port))
+
+def check_for_open_port(ip_address, port, queue):
     socket_to_check = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     result = socket_to_check.connect_ex((ip_address, int(port)))
     if result == 0:
-        print("Port " + port + " is open.")
+        queue.put(port)
     socket_to_check.close()
 
 def main():
